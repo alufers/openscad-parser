daff9cebc19fe65089bb18bdf45afecd866dc47b
alufers <alufers@wp.pl>
2020-12-07 14:34:12 +0100

    Try fixing docs builder

--- build-docs.sh
+++ build-docs.sh
@@ -1,2 +1,2 @@
-npm i typedoc typedoc-plugin-sourcefile-url
+npm i typedoc typescript typedoc-plugin-sourcefile-url
 ./node_modules/.bin/typedoc --sourcefile-url-prefix "https://github.com/alufers/openscad-parser/blob/master/"
