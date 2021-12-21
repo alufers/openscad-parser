66cd512f1b3a2cb486f7ad8920b561f3df0fe132
alufers <alufers@wp.pl>
2021-12-21 14:45:58 +0100

    fix: docs building

--- build-docs.sh
+++ build-docs.sh
@@ -1,2 +1,2 @@
-npm i typedoc typescript typedoc-plugin-sourcefile-url
-./node_modules/.bin/typedoc --sourcefile-url-prefix "https://github.com/alufers/openscad-parser/blob/master/"
+npm i typedoc typescript
+./node_modules/.bin/typedoc
