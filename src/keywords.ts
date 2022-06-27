33fc2a3afd8a967128320dacaf184b8a96a5b6a5
alufers <alufers@wp.pl>
2022-06-27 16:58:26 +0200

    add kewords docs

--- src/keywords.ts
+++ src/keywords.ts
@@ -20,4 +20,76 @@ const keywords: { [x: string]: TokenType } = {
   include: TokenType.Include,
 };
 
+const keywordDocumentation: { [x: keyof typeof keywords]: string } = {
+  true: "Represents the boolean value true.",
+  false: "Represents the boolean value false.",
+  undef: `Represents the undefined value. 
+
+It's the initial value of a variable that hasn't been assigned a value, and it is often returned as a result by functions or operations that are passed illegal arguments. `,
+  module: `Starts a module declaration.
+
+Usage:
+
+${"```scad"}
+module my_module(arg = "default") {
+  // module code
+}
+${"```"}
+`,
+  function: `Starts a function declaration.
+
+Usage:
+
+${"```scad"}
+function my_function (x) = x * x;
+
+// or for anonymous functions
+square = function (x) x * x;
+
+${"```"}
+`,
+  if: `Starts an if statement or expression.
+
+Usage:
+
+${"```scad"}
+if (x > 0) {
+  // do something
+}
+${"```"}
+`,
+  else: `Marks the beginning of an else block in an if statement.
+
+Usage:
+${"```scad"}
+if (x > 0) {
+  // if x is positive
+} else {
+  // if x is zero or negative
+}
+${"```"}
+`,
+  for: `Starts a for loop.
+
+Usage:
+
+${"```scad"}
+for ( i = [0 : 5] ){
+  rotate( i * 60, [1, 0, 0])
+  translate([0, 10, 0])
+  sphere(r = 10);
+}
+${"```"}
+`,
+  assert: `Starts an assert statement.
+
+Usage:
+
+${"```scad"}
+assert(x > 0, "x is not positive");
+
+${"```"}
+`,
+};
+
 export default keywords;
